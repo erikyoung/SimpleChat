e2c662246047da80a1565743ca8e93397c431a3f
erikyoung <erik.young@rmit.edu.vn>
2017-06-23 14:20:59 +0700

    added additional sender functionality

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,9 @@
 class Message < ApplicationRecord
-
+belongs_to :sender, class_name: 'User', foreign_key: 'sender_id'
+belongs_to :recipient, class_name: 'User', foreign_key: 'recepient_id'
 def sender
-	@user ||= User.find_by(id: sender_id)
+	@user ||= User.find_by(id: sender_id) || User.new(email: 'nobody@butt.you')
 end
+
+
 end
